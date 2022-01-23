c5824dcc87338e299c2c7867ec2a93566ef22f18
MaronNeAr <97968316+MaronNeAr@users.noreply.github.com>
2022-01-23 12:26:39 +0800

    Add files via upload

--- static/js/animate.js
+++ static/js/animate.js
@@ -1,41 +1,44 @@
 window.addEventListener('load', function() {
     var index = 0;
-    var arrow_ll = document.querySelector('.arrow-l');
-    var arrow_rr = document.querySelector('.arrow-r');
-    var mainImg = document.getElementById('mainImg');
-    var circleImg = document.getElementById('circle').querySelectorAll('img');
-    //var circleLi=document.getElementById('circle').querySelectorAll('li');
+    var arrow_ll = this.document.querySelector('.arrow-l');
+    var arrow_rr = this.document.querySelector('.arrow-r');
+    var mainImg = this.document.getElementById('mainImg');
+    var circleImg = this.document.getElementById('circle').querySelectorAll('img');
+    var imgHref = this.document.getElementById('imgHref');
+    var Href = ["https://www.beijing2022.cn/", "https://olympics.com/zh/video/beijing-2022-presents-one-year-to-go-show", "https://olympics.com/zh/video/gu-ailing-right-now-i-am-feeling-pretty-unstoppable", "https://olympics.com/zh/news/glossary-of-curling"];
     arrow_ll.onclick = function() {
         if (index == 0) index = 3;
         else index--;
-        mainImg.src = '/static/images/mainImg' + index + '.jpeg';
+        mainImg.src = '/static/images/mainImg/mainImg' + index + '.jpeg';
+        imgHref.href = Href[index];
         for (var i = 0; i < circleImg.length; i++) {
             if (circleImg[i].className == "current") {
-                circleImg[i].src = "/static/images/circle0.png";
+                circleImg[i].src = "/static/images/circle/circle0.png";
                 circleImg[i].className = "";
             }
         }
-        circleImg[index].src = "/static/images/circle1.png";
+        circleImg[index].src = "/static/images/circle/circle1.png";
         circleImg[index].className = "current";
     }
     arrow_rr.onclick = function() {
         index++;
         index %= 4;
-        mainImg.src = '/static/images/mainImg' + index + '.jpeg';
+        mainImg.src = '/static/images/mainImg/mainImg' + index + '.jpeg';
+        imgHref.href = Href[index];
         for (var i = 0; i < circleImg.length; i++) {
             if (circleImg[i].className == "current") {
-                circleImg[i].src = "/static/images/circle0.png";
+                circleImg[i].src = "/static/images/circle/circle0.png";
                 circleImg[i].className = "";
             }
         }
-        circleImg[index].src = "/static/images/circle1.png";
+        circleImg[index].src = "/static/images/circle/circle1.png";
         circleImg[index].className = "current";
     }
     for (var i = 0; i < circleImg.length; i++) {
         circleImg[i].onclick = function() {
             for (var i = 0; i < circleImg.length; i++) {
                 if (circleImg[i].className == "current") {
-                    circleImg[i].src = "/static/images/circle0.png";
+                    circleImg[i].src = "/static/images/circle/circle0.png";
                     circleImg[i].className = "";
                 }
             }
@@ -44,7 +47,8 @@ window.addEventListener('load', function() {
             for (var i = 0; i < circleImg.length; i++) {
                 if (circleImg[i].className == "current") index = i;
             }
-            mainImg.src = '/static/images/mainImg' + index + '.jpeg';
+            mainImg.src = '/static/images/mainImg/mainImg' + index + '.jpeg';
+            imgHref.href = Href[index];
         }
     }
 });
